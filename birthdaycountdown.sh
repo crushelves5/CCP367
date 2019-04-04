5f4f8c320a023c14cf2ad154c3ab13a38c1ab434
crushelves5 <betheladaghe@gmail.com>
2019-04-03 20:39:12 -0400

    Update birthdaycountdown.sh

--- birthdaycountdown.sh
+++ birthdaycountdown.sh
@@ -5,7 +5,44 @@ echo "Enter Birth month(Int) (1 = January..12 = December)"
 read birthmonth
 while [ $birthmonth != "STOP" ]
 do
-
+	echo "Enter Birth day(Int)"
+	read birthday
+	echo "Enter Birth year(YYYY)"
+	read birthyear
+	offset=0
+	if [ $# -gt 0 ]
+	then
+		offset=$1
+		offset=$((offset*24*60*60))
+		if [ $offset -gt 473040000 ]
+		then
+			offset=473040000
+		fi
+	fi
+	#Get current Date and apply offset if it exists, then extract the month and day
+	epochTime=$(date +%s)
+	currentTime=$((epochTime+offset))
+	month=$(date -d @$currentTime +%m)
+	day=$(date -d @$currentTime +%d)
+	year=$(date -d @$currentTime +%Y)
+	festive=""
+	if [ $month -eq 10 -a $day -eq 31 ]
+	then
+		echo "Happy Halloween"
+		festive=" Halloweeny"
+	elif [ $month -eq 12 -a $day -eq 25 ]
+	then
+		echo "Merry Christmas"
+		festive=" Christmasy"
+	elif [ $month -eq 3 -a $day -eq 17 ]
+	then
+		echo "Happy St. Patrick's Day"
+		festive=" Patricky"
+	elif [ $month -eq 7 -a $day -eq 1 ]
+	then
+		echo "Happy Canada Day"
+		festive=" Canada + You"
+ 	fi
 done
 
 exit 0
