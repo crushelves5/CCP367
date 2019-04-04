8d305cf58aad96d71c7bf5df06b399f3ad57fc3c
crushelves5 <betheladaghe@gmail.com>
2019-04-03 20:22:58 -0400

    Create birthdaycountdown.sh

---
+++ birthdaycountdown.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Welcome $USER"
+echo "Enter STOP into month to exit"
+echo "Enter Birth month(Int) (1 = January..12 = December)"
+read birthmonth
+while [ $birthmonth != "STOP" ]
+do
+
+done
+
+exit 0
